72942dcd1075c10febb92093d68622e14a4a255c
Tim Shannon <tim@townsourced.com>
2021-08-26 01:37:54 -0500

    More work on base project setup
    May move away from tailwind

---
+++ client/.eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+    "env": {
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential",
+        "plugin:@typescript-eslint/recommended"
+    ],
+	"parser": "vue-eslint-parser",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "parser": "@typescript-eslint/parser",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue",
+        "@typescript-eslint"
+    ],
+    "rules": {
+		"quotes": ["error", "double"],
+		"max-lines": ["error", 150],
+		"semi": ["error"],
+		"@typescript-eslint/explicit-module-boundary-types": "off",
+    },
+	"overrides": [
+    {
+      // enable the rule specifically for TypeScript files
+      "files": ["*.ts", "*.tsx"],
+      "rules": {
+        "@typescript-eslint/explicit-module-boundary-types": ["error"]
+      }
+    }
+  ]
+};
